bdecd67719a7b0932f2812ebf45fb627f26616ed
yjdjiayou <674032206@qq.com>
2019-11-13 14:01:30 +0800

    a

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,19 +1,27 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import actions from '../../store/actions/home';
-class Home extends Component{
-    componentWillMount(){
-        if(this.props.list.length == 0){
-            this.props.getHomeList();
-        }
+
+class Home extends Component {
+
+    // componentWillMount() {
+    //     console.log(666);
+    //     if (this.props.list.length == 0) {
+    //         this.props.getHomeList();
+    //     }
+    // }
+    
+    // componentDidMount 在服务器端是不执行的
+    componentDidMount(){
+        console.log(666);
     }
-    render(){
+    render() {
         return (
             <div className="row">
                 <div className="col-md-6 col-md-offset-3">
                     <ul className="list-group">
                         {
-                            this.props.list.map(item=>(
+                            this.props.list.map(item => (
                                 <li key={item.id} className="list-group-item">{item.name}</li>
                             ))
                         }
@@ -23,12 +31,13 @@ class Home extends Component{
         )
     }
 }
+
 Home = connect(
-    state=>state.home,
+    state => state.home,
     actions
 )(Home);
 // 此方法是用来异步加载数据的方法，用这个方法可以加载数据并且放到仓库中去
-Home.loadData = function(store){
-  return store.dispatch(actions.getHomeList());
+Home.loadData = function (store) {
+    return store.dispatch(actions.getHomeList());
 };
 export default Home;
