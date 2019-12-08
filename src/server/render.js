8ad5317b959010357e48c6a3ae3fa2fffb938504
yjdjiayou <674032206@qq.com>
2019-12-08 13:13:24 +0800

    新增注释

--- src/server/render.js
+++ src/server/render.js
@@ -72,7 +72,7 @@ export default function (req, res) {
                   // 服务端：组件初始化时会请求数据，请求的数据会存到服务端仓库中，然后组件使用数据显示相应内容
                   // 客户端：为了避免组件挂载时又一次的请求数据（当服务器端已经请求过数据并返回了有数据的内容） 
                   // 所以这里要获取下存在服务端仓库中的数据并作为初始值存到 window 中
-                  // 简称：数据的注水
+                  // 俗称：数据的脱水
                   window.context = {
                       state:${JSON.stringify(store.getState())}
                   }
--- src/store/index.js
+++ src/store/index.js
@@ -14,7 +14,7 @@ export function getClientStore() {
     // 服务端：组件初始化时会请求数据，请求的数据会存到服务端仓库中，然后组件使用数据显示相应内容
     // 客户端：为了避免组件挂载时又一次的请求数据（当服务器端已经请求过数据并返回了有数据的内容）
     // 取出服务端存到 window 中的仓库数据并作为初始值存到客户端仓库中
-    // 简称：数据的脱水
+    // 俗称：数据的注水
     let initState = window.context.state;
     return createStore(reducers, initState, applyMiddleware(thunk.withExtraArgument(clientRequest), logger));
 }
